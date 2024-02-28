3063596803ddd1e0cf863c0de8248407d164f01a
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-28 10:43:25 -0700

    ef

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Functionize Minecraft Fishing Start Code
 
 // Variables to store HTML Elements
-let fishBtn = document.getElementById('fish-btn');
-let charSelect = document.getElementById('character-select');
-let resultImg = document.getElementById('result-img');
-let numCodSpan = document.getElementById('num-cod');
-let numSalmonSpan = document.getElementById('num-salmon');
-let numTropicalSpan = document.getElementById('num-tropical');
-let numPufferSpan = document.getElementById('num-puffer');
+let fishBtn = document.getElementById("fish-btn");
+let charSelect = document.getElementById("character-select");
+let resultImg = document.getElementById("result-img");
+let numCodSpan = document.getElementById("num-cod");
+let numSalmonSpan = document.getElementById("num-salmon");
+let numTropicalSpan = document.getElementById("num-tropical");
+let numPufferSpan = document.getElementById("num-puffer");
 
 // Global Variables
 let numCod = 0;
@@ -16,43 +16,41 @@ let numTropical = 0;
 let numPuffer = 0;
 
 // Add Event Listener to Calculate Button
-fishBtn.addEventListener('click', fishBtnClicked);
+fishBtn.addEventListener("click", fishBtnClicked);
 
 // Event handler for fishBtn
 function fishBtnClicked() {
   // Check Selected Character
   let character = charSelect.value;
- // Catch fish based on character
-  if (character === 'steve') {
+  // Catch fish based on character
+  if (character === "steve") {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
     catchFish(0.7, 0.9, 0.95);
-  } else if (character === 'alex') {
+  } else if (character === "alex") {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
     catchFish(0.1, 0.2, 0.5);
-  }
+  } else character === "villager";
+  catchFish(0.25, 0.25, 0.25);
 }
 
 function catchFish(p1, p2, p3) {
   let randNum = Math.random();
   console.log(randNum);
-    if (randNum < p1) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < p2) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < p3) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
-  
+  if (randNum < p1) {
+    numCod++;
+    numCodSpan.innerHTML = numCod;
+    resultImg.src = "img/Raw-Cod.png";
+  } else if (randNum < p2) {
+    numSalmon++;
+    numSalmonSpan.innerHTML = numSalmon;
+    resultImg.src = "img/Raw-Salmon.png";
+  } else if (randNum < p3) {
+    numTropical++;
+    numTropicalSpan.innerHTML = numTropical;
+    resultImg.src = "img/Tropical-Fish.png";
+  } else {
+    numPuffer++;
+    numPufferSpan.innerHTML = numPuffer;
+    resultImg.src = "img/Pufferfish.png";
+  }
 }
-
-
